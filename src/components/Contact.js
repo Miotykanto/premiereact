ff045852ccc1bc87cc6c3838c4e96f2d3266b58d
Miotykanto <mirantotefy@gmail.com>
2019-03-05 16:48:16 +0300

    deuxieme envoi

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CONTACT</h2>
+        
+        <p>Vous pouvez contacter l'administrateur:&nbsp;<a href="http://www.twitter.com/miranto">Miranto</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
